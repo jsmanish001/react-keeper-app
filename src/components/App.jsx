8be930f4e85704fa2d540c5a13063d0470596860
jsmanish001 <66270532+jsmanish001@users.noreply.github.com>
2020-06-08 14:09:31 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+  console.log(notes);
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map(note => {
+        return <Note key={1} title={note.title} content={note.content} />;
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
